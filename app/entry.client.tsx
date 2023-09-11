5ace7de82d51d447177ac8018e7f29c44abab86b
Petech <petekshamitbeyli@gmail.com>
2023-09-11 19:21:53 +0300

    query-result-updates

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,17 +1,140 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React, { useState } from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  useQuery,
+  gql,
+} from "@apollo/client";
 import { RemixBrowser } from "@remix-run/react";
 import { hydrateRoot } from "react-dom/client";
 
+interface Country {
+  name: string;
+  native: string;
+  capital: string;
+  emoji: string;
+  currency: string;
+  size: string;
+  languages: {
+    code: string;
+    name: string;
+  }[];
+}
+
+// query for Apollo Client
+const COUNTRIES_QUERY = gql`
+  query GetCountries {
+    countries {
+      name
+      native
+      capital
+      emoji
+      currency
+      size
+      languages {
+        code
+        name
+      }
+    }
+  }
+`;
+
+// Filtering and Grouping
+function applyFilterAndGroup(
+  data: Country[],
+  filterInput: string
+): Country[] | Record<string, Country[]> {
+  const [searchTerm, groupBy] = filterInput.split(" group:");
+
+  // For filtering
+  const filteredData = data.filter((country) =>
+    searchTerm
+      ? country.name.toLowerCase().includes(searchTerm.toLowerCase())
+      : true
+  );
+
+  // For grouping
+  if (groupBy && groupBy === "size") {
+    const groupedData: Record<string, Country[]> = {};
+
+    filteredData.forEach((country) => {
+      const size = country.size || "Unknown";
+
+      if (!groupedData[size]) {
+        groupedData[size] = [];
+      }
+
+      groupedData[size].push(country);
+    });
+
+    return groupedData;
+  }
+
+  return filteredData;
+}
+
 function Client() {
-  const client = new ApolloClient({
-    cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
-    uri: "https://countries.trevorblades.com/graphql",
-  });
+  const [filterInput, setFilterInput] = useState("");
+  const { loading, error, data } = useQuery(COUNTRIES_QUERY); // useQuery, hook to fetch data
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    console.error("Apollo Client Error:", error);
+    return <div>Error: {error.message}</div>;
+  }
+
+  const filteredAndGroupedData = applyFilterAndGroup(
+    data?.countries || [],
+    filterInput
+  );
 
   return (
     <ApolloProvider client={client}>
+      <input
+        type="text"
+        placeholder="Search and group (e.g., search:tt group:size)"
+        value={filterInput}
+        onChange={(e) => setFilterInput(e.target.value)}
+      />
+      <div>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <>
+            {error !== null && error !== undefined ? (
+              <div>Error: {(error as Error).message}</div>
+            ) : (
+              <>
+                {Array.isArray(filteredAndGroupedData)
+                  ? filteredAndGroupedData.map((country) => (
+                      <div key={country.name}>{country.name}</div>
+                    ))
+                  : Object.keys(filteredAndGroupedData).map((size) => (
+                      <div key={size}>
+                        <strong>Size: {size}</strong>
+                        {filteredAndGroupedData[size].map((country) => (
+                          <div key={country.name}>{country.name}</div>
+                        ))}
+                      </div>
+                    ))}
+              </>
+            )}
+          </>
+        )}
+      </div>
+
       <RemixBrowser />
     </ApolloProvider>
   );
 }
+
+const client = new ApolloClient({
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
+  uri: "https://countries.trevorblades.com/graphql", // Make sure the URI is correct
+});
+
 hydrateRoot(document, <Client />);
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -2,40 +2,13 @@ import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
 
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink,
-} from "@apollo/client";
-
 export default function handleRequest(
-  request: Request, // Request type from the Fetch API
+  request: Request,
   responseStatusCode: number,
-  responseHeaders: Headers, // Headers type from the Fetch API
+  responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const requestHeaders: Record<string, string> = {};
-
-  request.headers.forEach((value, key) => {
-    requestHeaders[key] = value;
-  });
-
-  const client = new ApolloClient({
-    ssrMode: true,
-    cache: new InMemoryCache(),
-    link: createHttpLink({
-      uri: "https://countries.trevorblades.com/graphql", // Countries API URL
-      headers: requestHeaders,
-      credentials: request.credentials ?? "include", // or "same-origin" if your backend server is the same domain
-    }),
-  });
-
-  const App = (
-    <ApolloProvider client={client}>
-      <RemixServer context={remixContext} url={request.url} />
-    </ApolloProvider>
-  );
+  const App = <RemixServer context={remixContext} url={request.url} />;
 
   const markup = renderToString(App);
 
